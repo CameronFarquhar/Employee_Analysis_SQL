bbd93d667d3d18fef59feddf83ccbe9f70b1d43f
CameronFarquhar <cameronfarquhar@gmail.com>
2021-02-24 16:48:49 -0500

    added tables without keys

--- Employee_SQL/Schema.sql
+++ Employee_SQL/Schema.sql
@@ -2,4 +2,34 @@
 CREATE TABLE departments (
 	dept_no VARCHAR(5),
 	dept_name VARCHAR(25)
-	COMPOSITE )
+);
+	
+CREATE TABLE dept_emp (
+	dept_no VARCHAR(8),
+	emp_no INT
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(8),
+	emp_no INT
+);
+
+CREATE TABLE employees (
+	emp_no INTEGER PRIMARY KEY,
+	emp_title_id VARCHAR (8),
+	birth_date VARCHAR (10),
+	first_name VARCHAR (25),
+	last_name VARCHAR (25),
+	sex VARCHAR (8),
+	hire_date VARCHAR (10)
+);
+
+CREATE TABLE salaries (
+	emp_no INT,
+	salary INT
+);
+
+CREATE TABLE titles (
+	title_id VARCHAR(8),
+	Title VARCHAR (25)
+);
