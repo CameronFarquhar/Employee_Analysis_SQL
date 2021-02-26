fd445fb10c3995680dd8dc4bedd16cb26a8d69a8
CameronFarquhar <cameronfarquhar@gmail.com>
2021-02-26 14:49:24 -0500

    employee number, last name, first name, and department name

--- Employee_SQL/Employee_Analysis.sql
+++ Employee_SQL/Employee_Analysis.sql
@@ -21,3 +21,12 @@ ON dm.emp_no = e.emp_no
 JOIN departments AS d
 ON d.dept_no = dm.dept_no;
 
+-- department of each employee with the following information:
+-- employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+JOIN dept_emp AS de
+ON de.emp_no = e.emp_no
+JOIN departments AS d
+ON d.dept_no = de.dept_no;
